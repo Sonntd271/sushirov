b743a8bcc330fa161e5a30b97b85a7ab2007ae2f
Sonntd271 <itssonntd271@gmail.com>
2024-04-19 16:01:47 +0700

    Converted scanner to class

--- scanner/sushi.py
+++ scanner/sushi.py
@@ -1,125 +1,105 @@
-import time
 from gpiozero import Button, LED
-from signal import pause
-# -----------------------------------------------------
-red = LED(17)
-green = LED(27)
-blue = LED(22)
 
-#red.on()
-#time.sleep(10)
-
-PUL = LED(16)
-DIR = LED(20)
-ENA = LED(21) 
-button1 = Button(23)
-button2 = Button(24)
-switch1 = Button(12)
-switch2 = Button(6)
-
-def enable_button1():
-    global button1
-    button1.when_pressed = run_fwd #go to switch 1
-
-def disable_button1():
-    global button1
-    button1.when_pressed = None
-
-def enable_button2():
-    global button2
-    button2.when_pressed = run_bwd #go to switch 2
-
-def disable_button2():
-    global button2
-    button2.when_pressed = None 
-    
-def cancel_move2():
-	ENA.off()
-	PUL.off()
-	print("BUTTON 6 IS TRIGGERED")
-
-def cancel_move1():
-	ENA.off()
-	PUL.off()
-	print("BUTTON 12 IS TRIGGERED")
-	
-	
-def run_fwd():
-	ENA.on()
-	PUL.blink(0.001,0.001)
-	DIR.on()
-	print("pass")
-	
-def run_bwd():
-	ENA.on()
-	PUL.blink(0.001,0.001)
-	DIR.off()
-	print("pass")
-	
-button1.when_pressed = run_fwd #go to switch 1 and cancel 
-button2.when_pressed = run_bwd #go to switch 2
-switch1.when_released= cancel_move1
-switch2.when_released = cancel_move2
-
-
-
+FILE_PATH = "registered.txt"
 
 # -----------------------------------------------------
 
-def initialize():
-    prices = open("registered.txt", "r")
+class Scanner:
+    def __init__(self) -> None:
+        self.red = LED(17)
+        self.green = LED(27)
+        self.blue = LED(22)
 
-    prices_text = prices.read()
-    prices_list = prices_text.split("\n")
-    
-    prices_dict = {}
-    for price in prices_list[:-1]:
-        temp = price.split()
-        prices_dict[temp[0]] = int(temp[1])
-    
-    print(prices_dict)
-    return prices_dict
+        self.pul = LED(16)
+        self.dir = LED(20)
+        self.ena = LED(21) 
+        self.button1 = Button(23)
+        self.button2 = Button(24)
+        self.switch1 = Button(12)
+        self.switch2 = Button(6)
 
-prices_dict = {}
-total_price = 0
-total_count = 0
+        self.button1.when_pressed = self.run_fwd # go to switch 1 and cancel 
+        self.button2.when_pressed = self.run_bwd # go to switch 2
+        self.switch1.when_released = self.cancel_move1
+        self.switch2.when_released = self.cancel_move2
 
-def get_price(rfid):
-    if rfid not in prices_dict.keys():
-        print("cannot find rfid")
-        return 0
-    print("the price is", prices_dict[rfid])
-    return prices_dict[rfid]
+        self.prices_dict = {}
+        self.total_price = 0
+        self.total_count = 0
 
-def add_item(rfid):
-    global total_price
-    global total_count
-    total_price += get_price(rfid)
-    total_count += 1
-    print("The total cost so far is", total_price)
-    
+    def enable_button1(self):
+        self.button1.when_pressed = self.run_fwd #go to switch 1
 
-def reset_total():
-    global total_price
-    global total_count
-    
-    print("Resetting total price, which was", total_price)
-    print("Resetting total count, which was", total_count)
+    def disable_button1(self):
+        self.button1.when_pressed = None
 
-    total_price = 0
-    total_count = 0
-    
-def main(args):
-    print("Welcome to Bushido")
-    print("Please return the dished on thr tray")
-    global prices_dict
-    prices_dict = initialize()
-    while(True):
-        scanned_price = input()
-        add_item(scanned_price)
-    pause()
-    return 0
+    def enable_button2(self):
+        self.button2.when_pressed = self.run_bwd #go to switch 2
+
+    def disable_button2(self):
+        self.button2.when_pressed = None 
+        
+    def cancel_move2(self):
+        self.ena.off()
+        self.pul.off()
+        print("BUTTON 6 IS TRIGGERED")
+
+    def cancel_move1(self):
+        self.ena.off()
+        self.pul.off()
+        print("BUTTON 12 IS TRIGGERED")
+        
+    def run_fwd(self):
+        self.ena.on()
+        self.pul.blink(0.001,0.001)
+        self.dir.on()
+        print("pass")
+        
+    def run_bwd(self):
+        self.ena.on()
+        self.pul.blink(0.001,0.001)
+        self.dir.off()
+        print("pass")
+
+    def initialize(self):
+        with open(FILE_PATH, "r") as prices:
+            prices_list = prices.read().split("\n")
+            # print(prices_list)
+            
+            self.prices_dict = {}
+            for price in prices_list[:-1]:
+                id, price = price.split()
+                self.prices_dict[id] = int(price)
+            print(self.prices_dict)
+
+    def get_price(self, rfid):
+        if rfid not in self.prices_dict.keys():
+            print("cannot find rfid")
+            return 0
+        print("the price is", self.prices_dict[rfid])
+        return self.prices_dict[rfid]
+
+    def add_item(self, rfid):
+        self.total_price += self.get_price(rfid)
+        self.total_count += 1
+        print("The total cost so far is", self.total_price)
+        
+
+    def reset_total(self):
+        print("Resetting total price, which was", self.total_price)
+        print("Resetting total count, which was", self.total_count)
+
+        self.total_price = 0
+        self.total_count = 0
+
 
 if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+    scanner = Scanner()
+    print("Welcome to Bushido")
+    print("Please return the dished on the tray")
+    scanner.initialize()
+    while True:
+        scanned_price = input()
+        if scanned_price == "q":
+            break
+        scanner.add_item(scanned_price)
